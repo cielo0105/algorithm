4e86b8a4051f654826c4c1e540b134874a4a9551
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-02-26 13:51:36 +0900

    [Silver III] Title: 블로그, Time: 168 ms, Memory: 60696 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -3,18 +3,14 @@ visit = list(map(int,input().split()))
 v_max = sum(visit[:x])
 num = sum(visit[:x])
 cnt = 1
-start = 0
-end = x-1
-while end < n-1:
-   num = num - visit[start] + visit[end+1]
-   if num > v_max:
-      v_max = num
-      cnt = 1
-   elif num == v_max:
-      cnt += 1
-   
-   start += 1
-   end += 1
+
+for i in range(x,n):
+      num = num - visit[i-x] + visit[i]
+      if num > v_max:
+            v_max = num
+            cnt = 1
+      elif num == v_max:
+            cnt += 1
 
 if v_max == 0:
    print("SAD")
