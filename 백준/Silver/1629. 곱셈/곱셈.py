86de5f25025608c18194fd4689ba2211b2333a1c
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-04-14 19:19:36 +0900

    [Silver I] Title: 곱셈, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+a,b,c = map(int,input().split())
+def calc(a,b):
+   if b == 1:
+      return a%c
+   temp = calc(a,b//2)
+   if b%2 == 0:
+      return temp*temp%c
+   else:
+      return temp*temp*a%c
+   
+print(calc(a,b))
