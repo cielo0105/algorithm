7ed42cab367f64eccae6bc6381c5c9ae4df03d38
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-07-27 23:43:21 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 148 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String[][] arra = new String[n][m];
+        String[][] arrb = new String[n][m];
+        for(int i=0; i<n; i++){
+            arra[i] = br.readLine().split(" ");
+        }
+        for(int i=0; i<n; i++){
+            arrb[i] = br.readLine().split(" ");
+
+        }
+
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                sb.append(Integer.parseInt(arra[i][j])+Integer.parseInt(arrb[i][j])).append(" ");
+            }
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+    }
+}
