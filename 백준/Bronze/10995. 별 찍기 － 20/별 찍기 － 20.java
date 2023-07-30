beaa3708ce2cf84e9afaa14cc2447e7258107b6c
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-07-30 15:00:58 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 236 ms, Memory: 14856 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            if(i%2==0){
+                for(int j=0; j<n; j++){
+                    System.out.print("* ");
+                }
+            }
+            else{
+                for(int j=0; j<n; j++){
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
