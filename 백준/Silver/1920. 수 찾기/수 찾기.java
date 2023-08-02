0936398423f1647cdd9c8a49e73bd7b62707ddfc
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-08-02 10:38:56 +0900

    [Silver IV] Title: 수 찾기, Time: 1808 ms, Memory: 68620 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,39 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	static int[] arr;
-	static int n;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+	static int[] number;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		number = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			number[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
-		int m = sc.nextInt();
-		for (int i = 0; i < m; i++) {
-			int f = sc.nextInt();
-			System.out.println(find(f));
+		Arrays.sort(number);
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {
+			if(find(Integer.parseInt(st.nextToken()))) {
+				System.out.println(1);
+			}
+			else System.out.println(0);
 		}
-	}
-	public static int find(int num) {
-		int start = 0;
-		int end = n-1;
+	
+			
 		
-		while(start<=end) {
-			int mid = (start + end)/2;
-			if(arr[mid] == num) {
-				return 1;
+	}
+	static boolean find(int n) {
+		int s = 0;
+		int e = number.length-1;
+		while(s<=e) {
+			int mid = (s+e)/2;
+			if(number[mid] == n) return true;
+			if(number[mid] > n) {
+				e = mid-1;
 			}
-			else if(arr[mid]>num) {
-				end = mid-1;
-			}
-			else {
-				start = mid+1;
+			else if(number[mid] < n) {
+				s = mid+1;
 			}
 		}
-		return 0;
+		return false;
 	}
 }
