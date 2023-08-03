cc69e3c1b48a0fa4a5ce47f15b6e9eafa16a50bd
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-08-03 23:58:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int cnt = n/5;
-		int sugar = n%5;
-		int flag = 0;
-		while(true) {
-			if(sugar%3 == 0) {
-				cnt += sugar/3;
-				break;
-			}
-			else {
-				if(cnt == 0) {
-					flag = 1;
-					break;
-				}
-				cnt --;
-				sugar += 5;
-				
-			}
-		}
-		if(flag == 1) System.out.println(-1);
-		else System.out.println(cnt);
-	}
+public class Main
+{
+    static int n;
+    static int answer = -1;
+    static int flag = 0;
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // 3kg, 5kg 봉지 - 최대한 봉지 적게  (중복조합)
+        n = Integer.parseInt(br.readLine());
+        find(0,0);
+        System.out.println(answer);
+    }
+    static void find(int w, int cnt){ // w는 현재까지 무게
+        if(w>n || flag == 1) return;
+        if(w==n) {
+            answer = cnt;
+            flag = 1;
+            return;
+        }
+        find(w+5,cnt+1);
+        find(w+3,cnt+1);
+    }
 }
