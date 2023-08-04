31c6aeb077f1a64a5603b6d6bb87664b7865e446
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-08-04 16:41:53 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 23708 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,36 +4,32 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 /**
 
 @author 임현지
-@since 2023. 8. 3.
-@note
-
- * 제일 위에 있는 카드 버림 -> 제일 위에 있는 카드 제일 아래로 보냄
- * 하나 남을때까지 반복
- *
- */
+@since 2023. 8. 4.
+@note 
+*
+* queue
+*
+*/
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		Queue<Integer> queue = new ArrayDeque<>();
-		for(int i=1; i<=n; i++) {
+		for(int i=1; i<=N; i++) {
 			queue.add(i);
 		}
-		while(true) {
-			if(queue.size()==1) break;
-			queue.remove();
-			if(queue.size()==1) break;
-			queue.add(queue.poll());
+		while(queue.size()>1) { // 하나만 남은 경우 빠져나옴
+			queue.remove(); // 하나 버리기
+			queue.add(queue.poll()); // 맨 위의 카드 맨 아래로 보내기
 		}
 		System.out.println(queue.poll());
 	}
+	
 }
