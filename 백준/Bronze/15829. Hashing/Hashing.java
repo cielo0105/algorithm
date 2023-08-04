d72c2f4e77b6c6178236f3562dc0aa6a323811a4
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-08-04 16:21:09 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+/**
+
+@author 임현지
+@since 2023. 8. 4.
+@note 
+*
+* queue
+*/
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		int r = 31;
+		int M = 1234567891;
+		int sum = 0;
+		char[] word = br.readLine().toCharArray();
+		for(int i=0; i<L; i++) {
+			int ai = ((int)(word[i]-'a')+1);
+			
+			int ri = 1;
+			for(int j=0; j<i; j++) {
+				ri = (ri*r)%M;
+			}
+			sum += (ai*ri)%M;
+		}
+		System.out.println(sum);
+	}
+	
+}
