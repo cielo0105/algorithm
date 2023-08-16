bb8b01f02fbc54ae4f9ac334e843b1510bc39378
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-08-16 15:12:53 +0900

    [Gold II] Title: 빵집, Time: 332 ms, Memory: 51964 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,54 +1,56 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {	
-	static int[][] graph;
-	static int[] dx = {-1,0,1};
-	static int[] dy = {1,1,1};
-	static int r,c;
-	static int flag = 0;
-	static int cnt = 0;
-	public static void main(String[] args) throws IOException {
+/*
+ * R*C
+ * 오, 오위, 오아래
+ * .: 빈칸   x: 건물
+ * 왼쪽에서 오른쪽
+ * y=0 -> y=C-1
+ */
+public class Main {
+	static int R,C;
+	static char graph[][];
+	static boolean visited[][];
+	static int ans = 0;
+	static int flag;
+	static int[][] deltas = {{-1,1},{0,1},{1,1}};
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		graph = new int[r][c];
-		for (int i = 0; i < r; i++) {
-			String[] str = br.readLine().split("");
-			for (int j = 0; j < c; j++) {
-				if(str[j].equals(".")) graph[i][j] = 0;
-				else graph[i][j] = 1;
-			}
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		graph = new char[R][C];
+		visited = new boolean[R][C];
+		for(int i=0; i<R; i++) {
+			graph[i] = br.readLine().toCharArray();
 		}
-		for (int i = 0; i < r; i++) {
-			flag = 0;
-			dfs(i,0);
+		for(int i=0; i<R; i++) {
+				flag = 0;
+				find(i,0); // 왼쪽에서 출발
+			
 		}
-//		for (int[] g : graph) {
-//			System.out.println(Arrays.toString(g));
-//		}
-		System.out.println(cnt);
+		System.out.println(ans);
+		
 	}
-	
-	static void dfs(int x,int y) {
-		graph[x][y] = 2;
-		if(y == c-1) {
+	static void find(int a, int b) {
+		if(flag == 1) return;
+		if(b==C-1) {
+			ans++;
 			flag = 1;
-			cnt ++;
 			return;
 		}
-		for (int i = 0; i < 3; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if(nx>=0 && nx<r && ny<c && graph[nx][ny] == 0) {
-				if(flag == 1) return;
-				dfs(nx,ny);
+		int nx,ny;
+		for(int i=0; i<3; i++) {
+			nx = a + deltas[i][0];
+			ny = b + deltas[i][1];
+			if(nx>=0 && nx<R && ny>=0 && ny<C && graph[nx][ny]!='x' && !visited[nx][ny]) {
+				visited[nx][ny] = true;
+				find(nx,ny);
+				if(flag == 1) break;
+//				visited[nx][ny] = false;
 			}
 		}
 	}
