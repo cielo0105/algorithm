2d723273a5d5a5a1425ada4de3cae40c1099e0e2
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-08-18 11:08:48 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12276 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,52 +1,67 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+* 미로탐색 
+* N*M
+* 1: 이동 가능 
+* 0: 이동 불가
+*/
+
 public class Main {
-	static int[][] maze;
-	static int[] dx = {-1,0,1,0};
-	static int[] dy = {0,1,0,-1};
-	static boolean[][] visited;
-	static int n,m;
+	static int N,M;
+	static char[][] graph;
+	static int cnt = 0;
+	static int[][] deltas = {{-1,0},{0,1},{1,0},{0,-1}};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		maze = new int[n][m];
-		visited = new boolean[n][m];
-		for (int i = 0; i < n; i++) {
-			String[] l = br.readLine().split("");
-			for (int j = 0; j < m; j++) {
-				maze[i][j] = Integer.parseInt(l[j]);
-			}
+		StringTokenizer st = new  StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new char[N][M];
+		for(int i=0; i<N; i++) {
+			graph[i] = br.readLine().toCharArray();
 		}
-		bfs(0,0);
-		System.out.println(maze[n-1][m-1]);
+		System.out.println(find(0,0));
+		// 0,0 -> N-1,M-1
+		
 	}
-	public static void bfs(int i, int j) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {i,j});
-		visited[i][j] = true;
-		while(!queue.isEmpty()) {
-			int now[] = queue.poll();
-			for (int k = 0; k < 4; k++) {
-				int x = now[0] + dx[k];
-				int y = now[1] + dy[k];
-				if(x>=0 && x<n && y>=0 && y<m) {
-					if(!visited[x][y] && maze[x][y] == 1) {
-						visited[x][y] = true;
-						maze[x][y] = maze[now[0]][now[1]] + 1;
-						queue.offer(new int[] {x,y});
-					}
+	static int find(int a, int b) {
+		Queue<Point> q = new LinkedList<>();
+		Point point = new Point(a,b);
+		point.depth = 1;
+		q.add(point);
+		int nx,ny;
+		graph[a][b] = '0';
+		while(!q.isEmpty()) {
+			point = q.poll();
+			a = point.x;
+			b = point.y;
+			int depth = point.depth;
+			if(a==N-1 && b==M-1) return point.depth;
+			for(int i=0; i<4; i++) {
+				nx = a + deltas[i][0];
+				ny = b + deltas[i][1];
+				if(nx>=0 && nx<N && ny>=0 && ny<M && graph[nx][ny]!='0') {
+					point = new Point(nx,ny);
+					point.depth = depth + 1;
+					q.add(point);
+					graph[nx][ny] = '0';
 				}
 			}
 		}
+		return 0;
 	}
-	
+	public static class Point{
+		int x, y;
+		int depth;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
