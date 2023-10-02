45d53d716ae55c2dfcbd0fc2d36c106a8deaf91e
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-10-03 01:45:28 +0900

    [Silver I] Title: 경로 찾기, Time: 144 ms, Memory: 15364 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				for(int k=0; k<N; k++) {
+					if(map[j][i]==1 && map[i][k]==1) {
+						map[j][k] = 1;
+					}
+				}
+			}
+		}
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				sb.append(map[i][j]+" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
