f864af88b5ae4cac0daf02aaa312c8b7a7941aa2
Hyunji Lim <76719926+cielo0105@users.noreply.github.com>
2023-10-11 17:52:51 +0900

    [Gold III] Title: 파티, Time: 164 ms, Memory: 17028 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,45 +3,78 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N;
+	static int INF = 987654321;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-		int[][] dp = new int[N+1][N+1];
+		N = Integer.parseInt(st.nextToken()); // 학생 수
+		ArrayList<ArrayList<Info>> link1 = new ArrayList<>();
+		ArrayList<ArrayList<Info>> link2 = new ArrayList<>();
+		int[] dp1 = new int[N];
+		int[] dp2 = new int[N];
+		Arrays.fill(dp1, INF);
+		Arrays.fill(dp2, INF);
+		int M = Integer.parseInt(st.nextToken()); // 단방향 도로
+		int X = Integer.parseInt(st.nextToken())-1; // 목적지
 		int answer = 0;
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(i==j) dp[i][j] = 0;
-				else dp[i][j] = Integer.MAX_VALUE;
-			}
+		for (int i = 0; i < N; i++) {
+			link1.add(new ArrayList<>());
+			link2.add(new ArrayList<>());
 		}
-		for(int i=0; i<M; i++) {
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int time = Integer.parseInt(st.nextToken());
-			dp[from][to] = time;
+			int from = Integer.parseInt(st.nextToken()) - 1;
+			int to = Integer.parseInt(st.nextToken()) - 1;
+			int dist = Integer.parseInt(st.nextToken());
+			link1.get(from).add(new Info(to, dist));
+			link2.get(to).add(new Info(from,dist));
 		}
-		
-		for(int mid=1; mid<=N; mid++) {
-			for(int i=1; i<=N; i++) {
-				for(int j=1; j<=N; j++) {
-					if(dp[i][mid]==Integer.MAX_VALUE || dp[mid][j]==Integer.MAX_VALUE) continue;
-					dp[i][j] = Math.min(dp[i][j], dp[i][mid]+dp[mid][j]);
-				}
-			}
-		}
-		for(int i=1; i<=N; i++) {
-			answer = Math.max(answer,dp[i][X]+dp[X][i]);
+
+		dijkstra(X,link1,dp1);
+		dijkstra(X,link2,dp2);
+		for(int i=0; i<N; i++) {
+			answer = Math.max(answer, dp1[i]+dp2[i]);
 		}
 		System.out.println(answer);
 	}
+
 	
+
+	static void dijkstra(int start,ArrayList<ArrayList<Info>> link, int[] dp) {
+		PriorityQueue<Info> pq = new PriorityQueue<>();
+		pq.add(new Info(start, 0));
+		dp[start] = 0;
+		while (!pq.isEmpty()) {
+			Info info = pq.poll();
+			if (info.dist > dp[info.num])
+				continue;
+			for (Info next : link.get(info.num)) {
+				if (dp[next.num] > dp[info.num] + next.dist) {
+					dp[next.num] = dp[info.num] + next.dist;
+					pq.add(new Info(next.num, dp[next.num]));
+				}
+			}
+		}
+	}
+
+	static class Info implements Comparable<Info> {
+		int num, dist;
+
+		public Info(int num, int dist) {
+			this.num = num;
+			this.dist = dist;
+		}
+
+		@Override
+		public int compareTo(Info o) {
+			return this.dist - o.dist;
+		}
+	}
 }
